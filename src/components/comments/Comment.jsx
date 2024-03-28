96426bf04aa5636d810a2a0ed51cdc99a14fcd41
Yousif-Alaali-NV22012 <154865716+Yousif-Alaali-NV22012@users.noreply.github.com>
2024-03-28 08:29:54 +0000

    video 11 done

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -34,12 +34,12 @@ const Comment = ({
         src={images.PostProfileImage}
         alt="user profile"
         className="w-9 h-9 object-cover rounded-full"
-      />
-      <div className="flex-1 flex flex-col">
-        <h5 className="font-bold text-dark-hard text-xs">
-          {comment.user.name}
-        </h5>
-        <span className="text-xs text-dark-light">
+        />
+        <div className="flex-1 flex flex-col">
+          <h5 className="font-bold text-dark-hard text-xs lg:text-sm">
+            {comment.user.name}
+          </h5>
+          <span className="text-xs text-dark-light">
           {new Date(comment.createdAt).toLocaleDateString("en-US", {
             day: "numeric",
             month: "short",
